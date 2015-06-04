312f11987551717cf9621d15cb4846b939001a82
Kacper Gruszczyński <kacper.gruszczynski@wp.pl>
2015-06-04 23:38:38 +0200

    Dodano kontroler zapisywania nowej naprawy

--- application/_admin/controllers/ZarzadzaniezadaniamiController.php
+++ application/_admin/controllers/ZarzadzaniezadaniamiController.php
@@ -12,14 +12,59 @@
  * @author Kacper
  */
 class ZarzadzaniezadaniamiController extends AdminController {
-    public function init(){
+
+    public function init() {
         
     }
-    
-    public function indexAction(){
-        $Manage= new Managerepair();
-        $this->view->date=$Manage->getweekarray();
-        $this->view->mechanic=$Manage->countavailablemechanic();
-        $this->view->places=$Manage->countavailableplace();
+
+    public function indexAction() {
+        $Manage = new Managerepair();
+        $this->view->date = $Manage->getweekarray();
+        $this->view->mechanic = $Manage->countavailablemechanic();
+        $this->view->places = $Manage->countavailableplace();
     }
+
+    public function addrepairAction() {
+        $Manage = new Managerepair();
+        $date = $this->_getParam('date');
+        if ($date) {
+            $this->view->placestable = $Manage->getavailableplaces($date);
+            $this->view->mechanicstable = $Manage->getavailablemechanics($date);
+            $this->view->date=$date;
+        }
+    }
+
+    public function includecarAction() {
+        if ($this->_isPost()) {
+            $Manage = new Managerepair();
+            $this->view->date=  $this->_getParam('date');
+            $this->view->mechanicid = $this->_getPost('mechanic');
+            $this->view->placeid = $this->_getPost('place');
+            $cardata = $this->_getPost('car');
+            $clientdata=  $this->_getPost('client');
+            
+          $this->view->cars=$Manage->find($cardata,$clientdata);
+        }
+    }
+    
+    public function savedataAction(){
+        if($this->_isPost()){
+            $Manage = new Managerepair();
+            $date=  $this->_getParam('date');
+            $mechanicid=  $this->_getPost('mechanic');
+            $placeid=  $this->_getPost('place');
+            $carid=  $this->_getPost('carid');
+            $info=  $this->_getPost('info');
+            $status=  $this->_getPost('status');
+            $price=  $this->_getPost('price');
+            
+            if($Manage->saverepair($carid,$mechanicid,$placeid,$date,$info,$status,$price)){
+               echo "Informacje zapisane";
+            }
+            else{
+               
+            }
+        }
+    }
+
 }
--- application/models/Managerepair.php
+++ application/models/Managerepair.php
@@ -11,11 +11,7 @@
  *
  * @author Kacper
  */
-class Managerepair extends GeneralModelsController {
-
-    public function init() {
-        
-    }
+class Managerepair extends Basemodel {
 
     public function getweekarray() {
         $numberofday = 7;
@@ -31,17 +27,16 @@ class Managerepair extends GeneralModelsController {
         if ($this->setQuery($num)) {
             $this->fetchRow();
             $mechanic = $this->data;
-        }
-        else{
-           return false;
+        } else {
+            return false;
         }
         $weekarray = $this->getweekarray();
         $availablemechanic = array();
         foreach ($weekarray as $time) {
-           if( $this->setQuery("SELECT COUNT(*) FROM naprawa_pracownik WHERE Naprawa_ID=(SELECT id FROM naprawa WHERE Data= STR_TO_DATE('$time','%Y-%m-%d'))")){
-            $this->fetchRow();
-            $availablemechanic[] = (int)$mechanic - (int)$this->data['COUNT(*)'];
-           }
+            if ($this->setQuery("SELECT COUNT(*) FROM naprawa_pracownik WHERE Naprawa_ID IN(SELECT id FROM naprawa WHERE Data= STR_TO_DATE('$time','%Y-%m-%d'))")) {
+                $this->fetchRow();
+                $availablemechanic[] = (int) $mechanic - (int) $this->data['COUNT(*)'];
+            }
         }
         return $availablemechanic;
     }
@@ -51,27 +46,58 @@ class Managerepair extends GeneralModelsController {
         if ($this->setQuery($num)) {
             $this->fetchRow();
             $places = $this->data;
-        }
-        else{
-           return false;
+        } else {
+            return false;
         }
         $weekarray = $this->getweekarray();
         $availableplaces = array();
         foreach ($weekarray as $time) {
-           if( $this->setQuery("SELECT COUNT(*) FROM naprawa WHERE Data= STR_TO_DATE('$time','%Y-%m-%d')")){
-            $this->fetchRow();
-            $availableplaces[] = (int)$places - (int)$this->data['COUNT(*)'];
-           }
+            if ($this->setQuery("SELECT COUNT(*) FROM naprawa WHERE Data= STR_TO_DATE('$time','%Y-%m-%d')")) {
+                $this->fetchRow();
+                $availableplaces[] = (int) $places - (int) $this->data['COUNT(*)'];
+            }
         }
         return $availableplaces;
     }
-    
-    public function getavailablemechanics($date){
-        
+
+    public function getavailablemechanics($date) {
+        $query = "SELECT id, Imie, Nazw,nr_tel FROM pracownik WHERE id NOT IN( SELECT Pracownik_ID FROM naprawa_pracownik WHERE Naprawa_ID IN (SELECT id FROM naprawa WHERE Data= STR_TO_DATE('$date','%Y-%m-%d'))) AND Fun_Prac_ID=2";
+        if ($this->setQuery($query)) {
+            $this->fetchAll();
+            return $this->data;
+        } else
+            return false;
     }
-    
-    public function getavailableplaces($date){
-        
+
+    public function getavailableplaces($date) {
+        $query = "SELECT id, ladow, Nazw, przezn, wymiary, Posiad_tun FROM stanowisko WHERE id NOT IN (SELECT Stanowisko_ID FROM naprawa WHERE Data= STR_TO_DATE('$date','%Y-%m-%d'))";
+        if ($this->setQuery($query)) {
+            $this->fetchAll();
+            return $this->data;
+        } else
+            return false;
+    }
+
+    public function find(&$carinfo, &$clientinfo) {
+        $query = "SELECT * FROM samochod WHERE Marka='" . $carinfo['Marka'] . "' OR Model='" . $carinfo['Model'] . "' OR Rok_pr='" . $carinfo['Rok_pr'] . "' OR Klient_ID=(SELECT id FROM klient WHERE nazw='".$clientinfo."')";
+        if ($this->setQuery($query)) {
+            $this->fetchAll();
+            return $this->data;
+        } else {
+            return false;
+        }
+    }
+
+    public function saverepair($carid, $mechanicid, $placeid, $date, $info, $status, $price) {
+        $repair = "INSERT INTO naprawa(Data,Diagnoza,Status,Stanowisko_ID,Samochod_ID,Cena) VALUES (STR_TO_DATE('$date','%Y-%m-%d'),'$info','$status','$placeid','$carid','$price')";
+        if ($this->setQuery($repair)) {
+            $id = mysql_insert_id();
+            $repairid = "INSERT INTO naprawa_pracownik VALUES ('$id','$mechanicid')";
+            $this->setQuery($repairid);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
