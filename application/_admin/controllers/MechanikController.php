36bf7bbc1be53658766c9e42917e2428820597c2
PitterRup <pitter.rup@gmail.com>
2015-06-09 17:29:17 +0200

    panel dla mechanika

---
+++ application/_admin/controllers/MechanikController.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Mechanik
+ *
+ * @author Piotrek
+ */
+class MechanikController extends AdminController {
+
+    public function init() {
+
+    }
+
+    public function zadanialistAction() {
+        $this->_linkScript($this->baseUrl . '/public/template/styles/_admin/table&list.css');
+
+        $Manage = new Managerepair();
+        $this->view->repairs = $Manage->getrepairslist($this->sesField['id']);
+    }
+
+    public function showrepairAction() {
+        $this->_linkScript($this->baseUrl . '/public/template/styles/_admin/table&list.css');
+        $this->_linkScript($this->baseUrl . '/public/template/styles/_admin/form.css');
+        $repairid = $this->_getParam('repairid');
+        $Manage = new Managerepair();
+        $this->view->repair = $Manage->getrepair($this->sesField['id'], $repairid);
+    }
+
+    public function editrepairAction() {
+        $repairid = $this->view->repairid = $this->_getParam('repairid');
+        $Manage = new Managerepair();
+        if(!$this->_isPost()) {
+            $this->_headScript($this->baseUrl . '/public/js/_admin/form.js');
+            $this->_linkScript($this->baseUrl . '/public/template/styles/_admin/form.css');
+            $this->view->repair = $Manage->getrepair($this->sesField['id'], $repairid);
+        }
+        else {
+            $postdata = $this->_getPost('dane');
+            
+            if(!$Manage->editrepair($postdata, $repairid)) $this->msg(false, "Naprawa nie została zapisana.");
+            else $this->msg(true, "Naprawa została zapisana");
+            
+            $this->_request->goToAddress($this->directoryUrl . '/mechanik/zadanialist/type/msg', 0);
+        }
+    }
+}
